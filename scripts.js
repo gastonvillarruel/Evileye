ccebe201ea7a1e152b3e766146153bfbfb3bb9da
GV <gasnvillarruel@gmail.com>
2022-08-08 15:54:51 -0300

    funcionando

--- scripts.js
+++ scripts.js
@@ -2,87 +2,45 @@ addEventListener('click', e=>{
 	console.log(e);
 });
 
-// addEventListener('mousemove', e=>{
-// 	const root = document.documentElement;
-// 	let leftEyeX = e.clientX - 200;
-// 	let leftEyeY = e.clientY - 473;
-// 	let rightEyeX = e.clientX - 250;
-	
-// 	(leftEyeY<=-30 || leftEyeY>=30)?
-// 		(leftEyeY>=30)?
-// 			root.style.setProperty('--lefteyeY', `30px`)
-// 			:
-// 			root.style.setProperty('--lefteyeY', `-30px`)
-// 		:
-// 		root.style.setProperty('--lefteyeY', `${leftEyeY}px`);
 
-// 	(leftEyeX<=-16 || leftEyeX>=16)?
-// 		(leftEyeX>=16)?
-// 			root.style.setProperty('--lefteyeX', `16px`)
-// 			:
-// 			root.style.setProperty('--lefteyeX', `-16px`)
-// 		:
-// 		root.style.setProperty('--lefteyeX', `${leftEyeX}px`);
+const eyes = document.querySelectorAll('.pupil-container');
 
-// 	(leftEyeY<=-30 || leftEyeY>=30)?
-// 		(leftEyeY>=30)?
-// 			root.style.setProperty('--righteyeY', `30px`)
-// 			:
-// 			root.style.setProperty('--righteyeY', `-30px`)
-// 		:
-// 		root.style.setProperty('--righteyeY', `${leftEyeY}px`);
-
-// 	(rightEyeX<=-16 || rightEyeX>=16)?
-// 		(rightEyeX>=16)?
-// 			root.style.setProperty('--righteyeX', `16px`)
-// 			:
-// 			root.style.setProperty('--righteyeX', `-16px`)
-// 		:
-// 		root.style.setProperty('--righteyeX', `${rightEyeX}px`);
-
-
-// 	console.log(leftEyeX);
-
-
-// })
-
-
-
-const leftEye = document.querySelector('.lefteye');
-
-const leftEyePos = leftEye.getBoundingClientRect();
-	//devuelve un objecto (DOMRect) con datos sobre la posición del elemento relativa a la esquina superior izquierda de la ventana.
-console.dir(leftEye.clientWidth)
 
 addEventListener('mousemove', e=>{
 
-	// coordenadas del mouse
-	const mouseX = e.clientX;
-	const mouseY = e.clientY;
+	eyes.forEach(eye=>{
 
-	// coordenadas del ojo
-	const leftEyeX = leftEyePos.left + (leftEyePos.width/2);
-	const leftEyeY = leftEyePos.top + (leftEyePos.height/2);
+		const eyePos = eye.getBoundingClientRect();
+		//devuelve un objecto (DOMRect) con datos sobre la posición del elemento relativa a la esquina superior izquierda de la ventana.
 
-	//diferencia entre las anteriores (distancia)
-	const deltaX = leftEyeX - mouseX;
-	const deltaY = leftEyeY - mouseY;
+		// coordenadas del mouse
+		const mouseX = e.clientX;
+		const mouseY = e.clientY;
 
-	// posición del mouse en radianes (medida de ángulo)
-	const rad = Math.atan2(deltaY, deltaX);
+		// coordenadas del ojo
+		const eyeX = eyePos.left + (eyePos.width/2);
+		const eyeY = eyePos.top + (eyePos.height/2);
 
-	// conversión a grados (deg) por regla de 3, y redondeo
-	const deg = Math.round(rad*(180/Math.PI));
+		//diferencia entre las anteriores (distancia)
+		const deltaX = eyeX - mouseX;
+		const deltaY = eyeY - mouseY;
+
+		// posición del mouse en radianes (medida de ángulo)
+		const rad = Math.atan2(deltaY, deltaX);
+
+		// conversión a grados (deg) por regla de 3, y redondeo
+		const deg = Math.round(rad*(180/Math.PI));
 
 
-	console.log(leftEyeX, leftEyeY)
-	console.log(mouseX, mouseY)
-	console.log(deltaX, deltaY)
-	console.log(rad, deg)
+		eye.style.transform = `rotate(${deg}deg)`
+
+
+		console.log(eyeX, eyeY)
+		console.log(mouseX, mouseY)
+		console.log(deltaX, deltaY)
+		console.log(rad, deg)
+	})
 })
-
-
-console.log(innerWidth, innerHeight)
 
 
 
@@ -94,7 +52,6 @@ PI es la relación entre la longitud de la circunferencia y el diámetro. La cir
 Un Radián es el ángulo central de una circunferencia que abarca un arco de igual longitud que el radio (1rad es aproximadamente igual a 57.3°). 180° = PI rad, 360° = 2PI rad.
 
 Un cateto, es cualquiera de los dos lados menores de un triángulo rectángulo, los que conforman el ángulo recto. El lado de mayor medida se denomina hipotenusa.
-
 
 
 */
